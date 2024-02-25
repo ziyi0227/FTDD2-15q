754345737a9706da32b31ab9f3e289551f36a540
ziyi0227 <1206881089@qq.com>
2024-02-25 18:06:32 +0800

    标签栏导航

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,7 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: '首页', icon: 'dashboard' }
+      meta: { title: '首页', icon: 'dashboard', affix: true }
     }]
   },
 
