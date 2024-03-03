cea324963a0df33f716e53d31d4771dd46adbeb5
ziyi0227 <1206881089@qq.com>
2024-03-03 19:29:27 +0800

    职位信息页（未添加分页，未修改字段）

--- src/views/jobInfo/jobInfo.vue
+++ src/views/jobInfo/jobInfo.vue
@@ -1,15 +1,131 @@
 <template>
-  <div>
-    职位信息界面
+  <div class="container">
+    <!--搜索栏-->
+    <el-card>
+      <el-input placeholder="请输入内容" v-model="searchmodel.jobinput" class="input-with-select">
+        <el-select v-model="searchmodel.jobtype" slot="prepend" placeholder="请选择">
+          <el-option label="餐厅名" value="1"></el-option>
+          <el-option label="订单号" value="2"></el-option>
+          <el-option label="用户电话" value="3"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </el-card>
+    <el-card>
+      <el-table
+        :data="jobList"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline class="demo-table-expand">
+              <el-form-item label="商品名称">
+                <span>{{ props.row.name }}</span>
+              </el-form-item>
+              <el-form-item label="所属店铺">
+                <span>{{ props.row.shop }}</span>
+              </el-form-item>
+              <el-form-item label="商品 ID">
+                <span>{{ props.row.id }}</span>
+              </el-form-item>
+              <el-form-item label="店铺 ID">
+                <span>{{ props.row.shopId }}</span>
+              </el-form-item>
+              <el-form-item label="商品分类">
+                <span>{{ props.row.category }}</span>
+              </el-form-item>
+              <el-form-item label="店铺地址">
+                <span>{{ props.row.address }}</span>
+              </el-form-item>
+              <el-form-item label="商品描述">
+                <span>{{ props.row.desc }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="160">
+          <template>
+            <el-col :span="8">
+              <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+            </el-col>
+            <el-col :span="16">
+              <el-button type="primary" round>投递</el-button>
+            </el-col>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      searchmodel: {},
+      jobList: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }]
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+  .container {
+    margin: 15px 10px;
+  }
 
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+
+  .el-card {
+    margin-bottom: 10px;
+  }
+
+  .demo-table-expand {
+    font-size: 0;
+  }
+
+  .demo-table-expand label {
+    width: 100px;
+    color: #99a9bf;
+  }
+
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 100%;
+  }
 </style>
