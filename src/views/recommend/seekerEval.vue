7308873a24c6f67f546c48abcdcfe31478c0037e
ziyi0227 <1206881089@qq.com>
2024-03-23 20:31:49 +0800

    能力评价更新

--- src/views/recommend/seekerEval.vue
+++ src/views/recommend/seekerEval.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="job-container">
     <el-row gutter="16">
-      <el-col :span="10">
+      <el-col :span="14">
         <el-card class="job-card" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>我的收藏</span>
+          </div>
           <el-table
             :data="jobList"
             stripe
@@ -10,25 +13,41 @@
             @current-change="handleCurrentChange"
             style="width: 100%">
             <el-table-column
-              prop="date"
-              label="日期"
+              prop="jdTitle"
+              label="职位标题"
               width="100">
             </el-table-column>
             <el-table-column
-              prop="name"
-              label="姓名"
+              prop="company"
+              label="公司"
               width="100">
             </el-table-column>
             <el-table-column
-              prop="address"
-              label="地址">
+              prop="city"
+              label="城市"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              label="月薪">
+              <template slot-scope="{ row }">
+                {{ row.minSalary }} ~ {{ row.maxSalary }}
+              </template>
             </el-table-column>
           </el-table>
         </el-card>
       </el-col>
-      <el-col :span="14">
+      <el-col :span="10">
         <el-card  class="job-card" shadow="hover">
-
+          <!--  根据左边的选择行显示详细信息-->
+          <div v-if="selectedJob" class="job-details">
+            <h3>{{ selectedJob.jdTitle }}</h3>
+            <p>公司：{{ selectedJob.company }}</p>
+            <p>城市：{{ selectedJob.city }}</p>
+            <p>月薪：{{ selectedJob.minSalary }} ~ {{ selectedJob.maxSalary }}</p>
+          </div>
+          <div v-else>
+            请选择一行以查看详细信息
+          </div>
         </el-card>
       </el-col>
     </el-row>
@@ -48,18 +67,33 @@ export default {
         name: '王小虎',
         address: '上海市'
       }],
-      currentRow: null
+      currentRow: null,
+      selectedJob: null // 当前选择的职位详情信息
     }
   },
   methods: {
-    handleCurrentChange(val) {
-      this.currentRow = val
+    handleCurrentChange(currentRow) {
+      this.selectedJob = currentRow
     }
   }
 }
 </script>
 
 <style>
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+.clearfix{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .job-container {
   padding: 20px;
   position: relative;
