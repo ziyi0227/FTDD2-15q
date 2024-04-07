16cf8546973fde866f0a941ab0d92f0f90a13976
ziyi0227 <1206881089@qq.com>
2024-04-07 22:47:39 +0800

    更改简历界面

--- src/views/jobInfo/employGuide.vue
+++ src/views/jobInfo/employGuide.vue
@@ -6,151 +6,123 @@
         <div
           style="background-color: #FF6A00;
             height: 8px;
-            width: 24%;
+            width: 14%;
             margin-top: -18px;
-            margin-left: 58px;"
+            margin-left: 48px;"
         />
       </el-row>
-      <el-form ref="form" :model="resume" :rules="rules" label-width="100px">
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="专业" prop="major">
-              <el-input v-model="resume.major" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-          <el-row>
-            <el-col :span="12">
-              <el-form-item label="上传简历" prop="resumeFile">
-                <el-upload
-                  class="avatar-uploader"
-                  action=""
-                  :http-request="uploadResume"
-                  name="resume"
-                  :auto-upload="true"
-                  :show-file-list="false"
-                  style="display: inline-block;"
-                >
-                  <img v-if="resume.resumeFile" :src="resume.resumeFile" class="avatar">
-                  <i v-else class="el-icon-plus avatar-uploader-icon">编辑</i>
-                </el-upload>
+      <el-row gutter="30">
+        <el-col :span="14">
+          <el-form ref="form" :model="resume" :rules="rules" label-width="100px">
+            <el-row>
+              <el-form-item label="专业" prop="major">
+                <el-input v-model="resume.major" autocomplete="off" />
               </el-form-item>
-            </el-col>
-          </el-row>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="性别" prop="sex">
-              <el-radio-group v-model="resume.sex">
-                <el-radio label="1">男</el-radio>
-                <el-radio label="2">女</el-radio>
-                <el-radio label="3">保密</el-radio>
-              </el-radio-group>
+            </el-row>
+            <el-row>
+              <el-form-item label="性别" prop="sex">
+                <el-radio-group v-model="resume.sex">
+                  <el-radio label="1">男</el-radio>
+                  <el-radio label="2">女</el-radio>
+                  <el-radio label="3">保密</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="姓名" prop="name">
+                <el-input v-model="resume.name" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="电话" prop="phone">
+                <el-input v-model="resume.phone" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="年龄" prop="age">
+                <el-input v-model="resume.age" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="现居住地" prop="liveCity">
+                <el-input v-model="resume.liveCity" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="学历" prop="degree">
+                <el-input v-model="resume.degree" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="期望职类" prop="desireJdType">
+                <el-input v-model="resume.desireJdType" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="期望薪水" prop="desireJdSalaryId">
+                <el-input v-model="resume.desireJdSalaryId" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="期望行业" prop="desireJdIndustry">
+                <el-input v-model="resume.desireJdIndustry" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="期望工作城市" prop="desireCity">
+                <el-input v-model="resume.desireCity" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="开始工作时间" prop="start_work_date">
+                <el-input v-model="resume.startWorkDate" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="当前薪水" prop="currentSalaryId">
+                <el-input v-model="resume.currentSalaryId" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="最近工作行业" prop="curIndustry">
+                <el-input v-model="resume.curIndustry" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="最近工作职类" prop="curJdType">
+                <el-input v-model="resume.curJdType" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="工作经验" prop="experience">
+                <el-input v-model="resume.experience" type="textarea" autocomplete="off" />
+              </el-form-item>
+            </el-row>
+            <el-form-item>
+              <el-button type="warning" @click="saveResume()">确定</el-button>
+              <el-button @click="onCancel">取消</el-button>
             </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="姓名" prop="name">
-              <el-input v-model="resume.name" autocomplete="off"/>
+          </el-form>
+        </el-col>
+        <el-col :span="9">
+          <el-form>
+            <el-form-item label="上传简历" prop="resumeFile">
+              <el-upload
+                class="avatar-uploader"
+                action=""
+                :http-request="uploadResume"
+                name="resume"
+                :auto-upload="true"
+                :show-file-list="false"
+                style="display: inline-block;"
+              >
+                <img v-if="resume.resumeFile" :src="resume.resumeFile" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon">编辑</i>
+              </el-upload>
             </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="电话" prop="phone">
-              <el-input v-model="resume.phone" autocomplete="off"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="年龄" prop="age">
-              <el-input v-model="resume.age" autocomplete="off"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="现居住地" prop="liveCity">
-              <el-input v-model="resume.liveCity" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="学历" prop="degree">
-              <el-input v-model="resume.degree" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="期望职类" prop="desireJdType">
-              <el-input v-model="resume.desireJdType" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="期望薪水" prop="desireJdSalaryId">
-              <el-input v-model="resume.desireJdSalaryId" autocomplete="off"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="期望行业" prop="desireJdIndustry">
-              <el-input v-model="resume.desireJdIndustry" autocomplete="off"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="期望工作城市" prop="desireCity">
-              <el-input v-model="resume.desireCity" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="开始工作时间" prop="start_work_date">
-              <el-input v-model="resume.startWorkDate" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="当前薪水" prop="currentSalaryId">
-              <el-input v-model="resume.currentSalaryId" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="最近工作行业" prop="curIndustry">
-              <el-input v-model="resume.curIndustry" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="最近工作职类" prop="curJdType">
-              <el-input v-model="resume.curJdType" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="工作经验" prop="experience">
-              <el-input v-model="resume.experience" type="textarea" autocomplete="off" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-form-item>
-          <el-button type="warning" @click="saveResume()">确定</el-button>
-          <el-button @click="onCancel">取消</el-button>
-        </el-form-item>
-      </el-form>
+          </el-form>
+        </el-col>
+      </el-row>
     </el-card>
   </div>
 </template>
